2f25a2f64cdd2555319f2b49259dec4b5272285d
chrisRichar <50390808+chrisRichar@users.noreply.github.com>
2019-06-06 21:10:45 +1200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+import time
+
+import redis
+from flask import Flask
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+
+@app.route('/isPrime/')
+def prime1(num):
+    
+
+    return '{} is a prime\n'.format(num)
+
+
+@app.route('/primesStored/')
+def stored():
+    
+
+    return 'redis-stored primes\n'
